97d86b98c88cead4eb34358e895964ef6a780079
kodampranay <pranaykodam213@gmail.com>
2022-02-28 16:52:22 +0530

    create

--- create.php
+++ create.php
@@ -4,6 +4,10 @@ header('Access-Control-Allow-Methods: POST');
 header('Content-Type: application/json');
 $entityBody = json_decode(file_get_contents('php://input'));
 echo $entityBody.name;
+if(!empty($entityBody.name))
+{
+    echo $id=strtoupper(uniqid());
+}
 // include'config.php';
 // $query="SELECT * FROM crud";
 // $res=mysqli_query($conn,$query) or die('error query');
