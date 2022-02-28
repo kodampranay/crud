5cfc39753b148ee9bff41fc92be3b6e0e8334a30
kodampranay <pranaykodam213@gmail.com>
2022-02-28 16:20:58 +0530

    read

--- db-config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-$host="remotemysql.com";
-$db_user="M2nGoBaVzR";
-$db_pass="hlNNxLO1Ai";
-$db_name="M2nGoBaVzR";
-$conn=mysqli_connect($host,$db_user,$db_pass,$db_name) or die('error');
-?>
--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
-    header("Content-Type: application/json");
-    header("Access-Control-Allow-Methods: *");
-    header("Access-Control-Allow-Methods: GET");
-    include 'db-config.php';
-    $query="SELECT * FROM crud ";
-    $res=mysqli_query($conn,$query) or die('query error');
-    if(mysqli_num_rows($res)>0)
-    {   $data;
-        while($row=mysqli_fetech_assoc($res))
-        {
-            echo $row['name'];
-        }
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETEAccess-Control-Allow-Methods: POST, GET, OPTIONS, DELETE');
+header('Content-Type: application/json');
+include'config.php';
+$query="SELECT * FROM songsdb ORDER BY RAND()";
+$res=mysqli_query($conn,$query) or die('error query');
+if(mysqli_num_rows($res)>0)
+{
+    while($row=mysqli_fetch_assoc($res))
+    {
+        $data[]=$row;
     }
-    // echo json_encode($data);
+}
+
+echo json_encode($data);
 ?>
