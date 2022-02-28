efa0fcde1c9f30c7e5d5ab0b2402c9f96ad56786
kodampranay <pranaykodam213@gmail.com>
2022-02-28 16:00:57 +0530

    READ

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     header("Content-Type: application/json");
     header("Access-Control-Allow-Methods: *");
-    header("Access-Control-Allow-Methods: *");
+    header("Access-Control-Allow-Methods: GET");
     include 'db-config.php';
     $query="SELECT * FROM crud ";
     $res=mysqli_query($conn,$query) or die('query error');
