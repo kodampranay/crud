80c3bd1524518fb72d619bee34cf633790bb1f65
kodampranay <pranaykodam213@gmail.com>
2022-02-28 18:26:14 +0530

    create

--- create.php
+++ create.php
@@ -4,7 +4,7 @@ header('Access-Control-Allow-Methods: POST');
 header('Content-Type: application/json');
 $entityBody = file_get_contents('php://input');
 $obj_data=json_decode($entityBody,true);
-print_r($obj_data);
+echo $obj_data.name;
 // if(!empty($entityBody.name))
 // {
 //     // echo $id=strtoupper(uniqid());
