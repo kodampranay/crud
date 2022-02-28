bd59aa8a9953b00e175fa098a7fe259f09d7be5a
kodampranay <pranaykodam213@gmail.com>
2022-02-28 15:10:05 +0530

    get method

--- application/models/Crud.php
+++ application/models/Crud.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Welcome extends CI_Controller {
 public function get_user()
 {
-    return $this->db->get('crud');
+    return $this->db->get('crud')->result();
 }
 }
 ?>
