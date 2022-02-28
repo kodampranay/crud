a7a8b6125741da684e5df18d64b6eaf33ce9e6b0
kodampranay <pranaykodam213@gmail.com>
2022-02-28 16:41:49 +0530

    create

---
+++ create.php
@@ -0,0 +1,19 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
+header('Content-Type: application/json');
+$entityBody = file_get_contents('php://input');
+echo $entityBody;
+// include'config.php';
+// $query="SELECT * FROM crud";
+// $res=mysqli_query($conn,$query) or die('error query');
+// if(mysqli_num_rows($res)>0)
+// {
+//     while($row=mysqli_fetch_assoc($res))
+//     {
+//         $data[]=$row;
+//     }
+// }
+
+// echo json_encode($data);
+// ?>
