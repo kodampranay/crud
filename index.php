ed5166025ece3207d5a58abfded17fac69fc36e0
kodampranay <pranaykodam213@gmail.com>
2022-02-28 15:52:21 +0530

    read

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-
+    // header("Content-Type: application/json");
+    // header("Access-Control-Allow-Methods: *");
+    // header("Access-Control-Allow-Methods: *");
+    include 'db-config.php';
+    // $query="SELECT * FROM crud ";
+    // $res=mysqli_query($conn,$query) or die('query error');
+    // if(mysqli_num_rows($res)>0)
+    // {   $data;
+    //     while($row=mysqli_fetech_assoc($res))
+    //     {
+    //         $data[]=$row;
+    //     }
+    // }
+    // echo json_encode($data);
 ?>
