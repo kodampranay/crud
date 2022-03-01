cb114ba4a055ded4d33ab650a5758f0472c859aa
kodampranay <pranaykodam213@gmail.com>
2022-03-01 15:08:10 +0530

    update

--- update.php
+++ update.php
@@ -3,25 +3,25 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: PUT');
 header('Content-Type: application/json');
 echo $entityBody = json_decode(file_get_contents('php://input'));
-if(!empty($entityBody->name)&&!empty($_GET['id'])&&strlen($entityBody->name)<256)
-{
-    $id=$_GET['id'];
-include'config.php';
-$query="UPDATE  crud SET name='{$entityBody->name}' WHERE id='{$id}'";
-if(mysqli_query($conn,$query) or die('error query'))
-{
-    echo json_encode(array('message'=>'ok'));
-}
+// if(!empty($entityBody->name)&&!empty($_GET['id'])&&strlen($entityBody->name)<256)
+// {
+//     $id=$_GET['id'];
+// include'config.php';
+// $query="UPDATE  crud SET name='{$entityBody->name}' WHERE id='{$id}'";
+// if(mysqli_query($conn,$query) or die('error query'))
+// {
+//     echo json_encode(array('message'=>'ok'));
+// }
 
-else{
-    echo json_encode(array('message'=>'error'));
-}
+// else{
+//     echo json_encode(array('message'=>'error'));
+// }
 
-}
+// }
 
-else{
-    echo json_encode(array('message'=>'went wrong'));
-}
+// else{
+//     echo json_encode(array('message'=>'went wrong'));
+// }
 
 
 
