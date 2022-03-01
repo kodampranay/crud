94322faf6f94f1a5497f8e04d3074278f49fd8c3
kodampranay <pranaykodam213@gmail.com>
2022-03-01 15:02:41 +0530

    update

--- update.php
+++ update.php
@@ -3,21 +3,26 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: PUT');
 header('Content-Type: application/json');
 $entityBody = json_decode(file_get_contents('php://input'));
-$id=$_GET['id'];
-include'config.php';
-$query="SELECT * FROM crud WHERE id='{$id}'";
-$res=mysqli_query($conn,$query) or die('error query');
-if(mysqli_num_rows($res)>0)
+if(!empty($entityBody->name)&&!empty($_GET['id'])&&strlen($entityBody->name)<256)
 {
-    while($row=mysqli_fetch_assoc($res))
-    {
-        $data[]=$row;
-    }
-    echo json_encode(array('message'=>$data));
+    $id=$_GET['id'];
+include'config.php';
+$query="UPDATE  crud SET name='{$entityBody->name}' WHERE id='{$id}'";
+if(mysqli_query($conn,$query) or die('error query'))
+{
+    echo json_encode(array('message'=>'ok'));
 }
+
 else{
     echo json_encode(array('message'=>'error'));
 }
+
+}
+
+else{
+    echo json_encode(array('message'=>'went wrong'));
+}
+
 
 
 ?>
